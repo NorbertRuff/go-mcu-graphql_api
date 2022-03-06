d4d028b05ce6fe20899f5ef45dd4671d8019bac2
Norbert Ruff <norbi@apexlab.io>
2022-03-06 19:28:21 +0100

    [add] graphql resolvers

---
+++ graphql/resolver.go
@@ -0,0 +1,13 @@
+package graphql
+
+import "github.com/norbertruff/go-graphql/graphql/generated/models"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	movies []*models.Movie
+	shows  []*models.TvShow
+	links  []*models.Link
+}
