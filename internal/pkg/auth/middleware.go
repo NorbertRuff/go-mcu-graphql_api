1701d05b82086f279594970579e396d1ebecdccc
Norbert Ruff <norbi@apexlab.io>
2022-03-06 19:35:38 +0100

    [add] auth

---
+++ internal/pkg/auth/middleware.go
@@ -0,0 +1,57 @@
+package auth
+
+import (
+	"context"
+	"github.com/norbertruff/go-graphql/graphql/generated/models"
+	"github.com/norbertruff/go-graphql/internal/services/users"
+	"net/http"
+)
+
+var userCtxKey = &contextKey{"user"}
+
+type contextKey struct {
+	name string
+}
+
+func Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header := r.Header.Get("Authorization")
+
+			// Allow unauthenticated users in
+			if header == "" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			//validate jwt token
+			tokenStr := header
+			username, err := ParseToken(tokenStr)
+			if err != nil {
+				http.Error(w, "Invalid token", http.StatusForbidden)
+				return
+			}
+
+			// create user and check if user exists in db
+			user := models.User{Username: username}
+			_, err = user_service.GetUserIdByUsername(username)
+			if err != nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+			//user.ID = strconv.Itoa(id)
+			// put it in context
+			ctx := context.WithValue(r.Context(), userCtxKey, &user)
+
+			// and call the next with our new context
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// ForContext finds the user from the context. REQUIRES Middleware to have run.
+func ForContext(ctx context.Context) *models.User {
+	raw, _ := ctx.Value(userCtxKey).(*models.User)
+	return raw
+}
